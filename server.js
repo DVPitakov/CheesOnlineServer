e34bcb5c44dd78ea767ff581d24373e333a133bb
Dmitry <Dmitry>
2016-09-20 00:41:56 +0300

    commit

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("Server are working");
-});
-server.listen(80);
-
-
-
 var WebSocketServer = new require('ws');
 var games = {};
 games[0] = new Game();
