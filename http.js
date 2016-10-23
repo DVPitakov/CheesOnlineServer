5139d9d16c966f9ad1a11fb99d896b479d8835bd
Dmitry <Dmitry>
2016-10-23 12:23:23 +0300

    not activ users deleter

--- http.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("Server are working");
-});
-server.listen(2000);
-export server = "server"
--- server.js
+++ server.js
@@ -1,48 +1,86 @@
 var WebSocketServer = new require('ws');
-var games = {};
+var games = [];
 games[0] = new Game();
 var lastId = 0;
 
+
+ function stepMessage(data) {
+ 	return {
+		action: "step",
+		oldPos: data.oldPos || 0,
+ 		newPos: data.newPos || 0
+	}
+  }
+  function colorMessage(data) {
+	return {
+        	action: "color",
+        	userColor: data.userColor || 0
+	}
+  }
+  function pawTransMessage(data) {
+	return {
+		action: "pawTrans",
+        	figure: data.figure || 0,
+        	pos: data.pos || 0
+	}
+  }
+  function gameEndMessage(data) {
+	return {
+        	action: "gameEnd",
+		couse: data.couse || 0,
+		data: data.data || 0
+	}
+  }
+  function howAreYouMessage(data) {
+	return {
+        	action: "howAreYou",
+		health: data.health || "fine"
+	}
+  }
+
 function Game() {
-  this.users = [null,null];
+  this.users = [null, null];
 }
 
+setInterval(function() {
+	console.log("hey hey hey i am here ororor");
+	games.forEach(function(el){
+		console.log("hey hey hey i am here");
+		if (null != el.users[0] && null != el.users[0].isAlive && null != el.users[1] && null != el.users[1].isAlive) {
+			for(var i = 0; i < 2; i++) {
+				if (null != el.users[i] && el.users[i].isAlive) {
+					el.users[i].isAlive = false;
+				}
+				else {
+					try {
+						el.users[i].send(JSON.stringify(gameEndMessage({couse: 45})));
+						el.users[i].close();
+					}
+					catch(err) {
+					}
+					try {
+						el.users[1 - i].send(JSON.stringify(gameEndMessage({couse: 25})));
+						el.users[1 - i].close();
+					}
+					catch(err) {
+					}
+					el.users = [null, null];
+				}
+			}
+		}
+	})
+}, 10000);
 var webSocketServer = new WebSocketServer.Server({port: process.env.PORT || 8081});
 webSocketServer.on('connection', function(ws) {
-  var stepMessage = {
-	action: "step",
-	oldPos: 0,
- 	newPos: 0
-  };
-  var colorMessage = {
-        action: "color",
-        userColor: 0
-  };
-  var pawTransMessage = {
-	action: "pawTrans",
-        figure: 0,
-        pos: 0
-  };
-  var gameEndMessage = {
-        action: "gameEnd",
-	couse: 0,
-	data: 0
-  };
-  var howAreYouMessage = {
-        action: "howAreYou",
-	health: "fine"
-  };
   var color = 0;
   var gameId = lastId;
   if (games[gameId].users[0]) {
 	console.log("action is colorMessage")
 	games[gameId].users[1] = ws;
-        colorMessage.userColor = 0;
-	games[lastId].users[0].send(JSON.stringify(colorMessage));
-	console.log(JSON.stringify(colorMessage));
-        colorMessage.userColor = 1;
-	games[lastId].users[1].send(JSON.stringify(colorMessage));
-	console.log(JSON.stringify(colorMessage));
+	games[lastId].users[0].send(JSON.stringify(colorMessage({userColor: 0})));
+	console.log(JSON.stringify(colorMessage({userColor: 0})));
+	games[lastId].users[1].send(JSON.stringify(colorMessage({userColor: 1})));
+	console.log(JSON.stringify(colorMessage({userColor: 1})));
 	color = 1;
 	lastId += 1;
 	games[lastId] = new Game();
@@ -52,34 +90,49 @@ webSocketServer.on('connection', function(ws) {
 	color = 0;
   }  
 
-  
   ws.on('message', function(userMsg) {
-    var msg = JSON.parse(userMsg);
-    if(msg.action) {
+    try {
+	var msg = JSON.parse(userMsg);
+	games[gameId].users[color].isAlive = true;
+    if(null != msg && null != msg.action) {
 	console.log("H" + msg.action + "H");
 	console.log((msg.action + "") == "step");
     	if(msg.action.toString() == "step") {
 		console.log("action is step");
     		stepMessage.oldPos = msg.oldPos;
     		stepMessage.newPos = msg.newPos;
-    		games[gameId].users[1 - color].send(JSON.stringify(stepMessage));
+    		games[gameId].users[1 - color].send(JSON.stringify(stepMessage(msg)));
    	 }
    	 else if(msg.action.toString() == "pawTrans") {
 		console.log("action is pawTrans");
-		pawTransMessage.figure = msg.figure;
-		pawTransMessage.pos = msg.pos;
-		games[gameId].users[1 - color].send(JSON.stringify(pawTransMessage));
+		games[gameId].users[1 - color].send(JSON.stringify(pawTransMessage(msg)));
    	 }
 	 else if(msg.action.toString() == "gameEnd") {
 		console.log("action is gameEnd");
-		gameEndMessage.couse = msg.couse;
-		gameEndMessage.data = msg.data;
-		games[gameId].users[1 - color].send(JSON.stringify(gameEndMessage));
+		games[gameId].users[1 - color].send(JSON.stringify(gameEndMessage(msg)));
+		try {
+			games[gameId].users[1 - color].close();
+		}
+		catch(e) {
+			console.log("hopa1");
+		}
+		try {
+			games[gameId].users[color].close();
+		}
+		catch(e) {
+			console.log("hopa2");
+		}
+		games[gameId] = [null,null];
 	 }
 	 else if(msg.action.toString() == "howAreYou") {
-		games[gameId].users[color].send(JSON.stringify(howAreYouMessage));
+		console.log("how are you message was get");
+		games[gameId].users[color].send(JSON.stringify(howAreYouMessage(msg)));
 	 }
     }
+ }
+    catch(err) {
+	console.log("wrong message");
+    }
   });
-  ws.on('close', function() {})
+  ws.on('close', function() {});
 });
