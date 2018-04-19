2a9ee42e46dda89414fa285c5a7bb653a5bb4c2b
dmitry <dvpitakov@gmail.com>
2018-04-19 03:08:46 +0300

    bag fix

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+var xelement = require('xelement');
 stepMessage = require('./msgGenerator.js').stepMessage;
 colorMessage =  require('./msgGenerator.js').colorMessage;
 pawTransMessage = require('./msgGenerator.js').pawTransMessage;
@@ -5,24 +6,26 @@ gameEndMessage = require('./msgGenerator.js').gameEndMessage;
 howAreYouMessage = require('./msgGenerator.js').howAreYouMessage;
 BoardLogic = require('./rools').BoardLogic;
 class Game {
-	constructor() {
+	constructor(game_id) {
 		console.log('Here');
+        this.game_id = game_id;
 		this.board = new BoardLogic();
-		this.users = [0, 0];
+        this.users_colors = new Map();
+		this.users_data_for_send = [[], []];
 		this.userActive = [true, true];
 		this.isStarted = false;
 		this.cur = 0;
 	}
-	addUser(ws) {
-		this.users[this.cur] = ws;
-		this.cur += 1;		
+	addUser(user_id) {
+		this.users_colors[user_id] = this.cur;
+		this.cur += 1;
 	}
 	isActive() {
 		return (this.userActive[0] && this.userActive[1]);
 	}
 	run() {
-		this.users[0].send(JSON.stringify(colorMessage({userColor: 0})));
-		this.users[1].send(JSON.stringify(colorMessage({userColor: 1})));
+		this.users_data_for_send[0].push(colorMessage({userColor: 1}));
+		this.users_data_for_send[1].push(colorMessage({userColor: 0}));
 		this.isStareted = true;
 	}
 	started() {
@@ -34,44 +37,40 @@ class Game {
 	}
 	disconnect() {
 		if(this.userActive[0]) {
-			this.users[0].send(gameEndMessage({couse: 45}));
+			this.users_data_for_send[0].push(gameEndMessage({couse: 45}));
 		}
 		else if (this.userActive[1]) {
-			this.users[1].send(gameEndMessage({couse: 45}));
+			this.users_data_for_send[1].push(gameEndMessage({couse: 45}));
 		}
-		this.users[0].close();
-		this.users[1].close();
-		this.users[0] = null;
-		this.users[1] = null;
 	}
 	nextStep(data, color) {
 		if (data != null && data.oldPos != null && data.newPos != null) {
 			let res = this.board.moveFig(data.oldPos, data.newPos);
 			if (res == 0 || res == 4) {
-    				this.users[1 - color].send(JSON.stringify(stepMessage(data)));
+    				this.users_data_for_send[1 - color].push(stepMessage(data));
 			}
 		}
 	}
 	gameEnd(data, color) {
-		this.users[1 - color].send(JSON.stringify(gameEndMessage(data)));
-		this.users[1-color].close();
-		this.users[color].close();
+		this.users_data_for_send[1 - color].push(gameEndMessage(data));
 	}
 	pawTrans(data, color) {
-		this.users[1 - color].send(JSON.stringify(pawTransMessage(data)));
-	}
-	howAreYou(data, color) {
-		this.users[color].send(JSON.stringify(howAreYouMessage(data)));
+		this.users_data_for_send[1 - color].push(pawTransMessage(data));
 	}
 	nichia(data, color) {
-		this.users[1 - color].send(JSON.stringify({action: "nichia"}));
+		this.users_data_for_send[1 - color].push({action: "nichia"});
 	}
-	sayYes(color){
-		this.users[1 - color].send(JSON.stringify({action: "sayYes"}));
+	sayYes(color) {
+		this.users_data_for_send[1 - color].push({action: "sayYes"});
 	}
-	sayNot(color){
-		this.users[1 - color].send(JSON.stringify({action: "sayNot"}));
+	sayNot(color) {
+		this.users_data_for_send[1 - color].push({action: "sayNot"});
 	}
+    getUpgrade(color) {
+        let buf = this.users_data_for_send[color];
+        this.users_data_for_send[color] = [];
+        return xelement.ParseJson(buf).toXmlString();
+    }
 	 
 }
 exports.Game = Game;
--- main.js
+++ main.js
@@ -1,56 +1,88 @@
-var WebSocketServer = new require('ws');
+var http = require('http');
+var xelement = require('xelement');
+var url = require('url');
 var Game = require('./game.js').Game;
 var Disconnector = require('./disconnector.js').Disconnector;
-var games = [];
+
+var games = new Map();
+var openGames = [];
+
 var lastId = 0;
-var webSocketServer = new WebSocketServer.Server({port: process.env.PORT || 8081});
-var disconnector = new Disconnector(games, 10000);
-webSocketServer.on('connection', function(ws) {
-	console.log(JSON.stringify(ws.upgradeReq.connection.remotePort));
-  console.log('here');
-  var color = 0;
-  var gameId = lastId;
-  var game = games[gameId];
-  if (games[gameId] != null) {
-	game.addUser(ws);
-	game.run();
-	color = 1;
-	lastId += 1;
+
+http.createServer(function (req, res) {
+  console.log("new client!!!");
+  let client_url = url.parse(req.url, true);
+  let game_id = client_url.query.game_id;
+  let user_id = client_url.query.user_id;
+  let user_msg = client_url.query.user_msg;
+  console.log(game_id);
+  console.log(user_id);
+  
+  
+  if (game_id && user_id && game_id.length > 0 && user_id.length > 0) {
+    let result = doGameLogic(game_id, user_id, user_msg);
+    console.log(result);
+    res.write(result);
   }
   else {
-	game = new Game();
-	games[gameId] = game;
-	game.addUser(ws);
-	console.log('begin');
-	color = 0;
-  }  
+    console.log("game not started");
+    let generated_user_id = "A" + Math.random();
+    let game_id;
+    let game;
+    if(openGames.length > 0) {
+         console.log("free game exists");
+         game = openGames.pop();
+         game_id = game.game_id;
+         game.addUser(generated_user_id);
+         games[game_id] = game;
+         game.run(); 
+    }
+    else {
+        console.log("free game was created");
+        game_id = "B" + Math.random();
+        game = new Game(game_id);
+        game.addUser(generated_user_id);
+        openGames.push(game);
+    }
+    console.log(xelement.ParseJson([{action: "userKey", user_id: generated_user_id, game_id: game_id}]).toXmlString());
+    res.write(xelement.ParseJson([{action: "userKey", user_id: generated_user_id, game_id: game_id}]).toXmlString());
+  }
+  res.end(); //end the response
+})
+.listen(8080); //the server object listens on port 8080
 
-  ws.on('message', function(userMsg) {
-	var msg = JSON.parse(userMsg);
-	game.userActive[color]  = true;
-    	if(null != msg && null != msg.action) {
-    		if(msg.action.toString() == "step") {
-			game.nextStep(msg, color);
-   	 	}
-   	 	else if(msg.action.toString() == "pawTrans") {
-			game.pawTrans(msg, color);
-   	 	}
-	 	else if(msg.action.toString() == "gameEnd") {
-			game.gameEnd(msg, color);
-	 	}
-	 	else if(msg.action.toString() == "howAreYou") {
-			game.howAreYou(msg, color);
-	 	}
-	 	else if(msg.action.toString() == "nichia") {
-			game.nichia(msg, color);
-	 	}
-		else if(msg.action.toString() == "sayYes") {
-			game.sayYes(color);
-	 	}
-		else if(msg.action.toString() == "sayNot") {
-			game.sayNot(color);
-	 	}
-    	}
-  });
-  ws.on('close', function() {});
-});
+
+function doGameLogic(game_id, user_id, user_msg) {
+    console.log("in do section");
+    var game = games[game_id];
+    if(!game) return xelement.ParseJson([{action: "nothing"}]).toXmlString();
+    console.log("game exists");
+    var color = game.users_colors[user_id];
+    console.log("COLOR: " + color);
+    var msg = JSON.parse(user_msg);
+    game.userActive[color] = true;
+    let result = xelement.ParseJson([{action: "nothing"}]).toXmlString();
+    console.log("MSG_ACTION: " + msg.action.toString());
+    if(msg.action.toString() == "step") {game.nextStep(msg, color);}
+    else if(msg.action.toString() == "pawTrans") {
+	    game.pawTrans(msg, color);
+    }
+    else if(msg.action.toString() == "gameEnd") {
+	    game.gameEnd(msg, color);
+    }
+    else if(msg.action.toString() == "nichia") {
+	    game.nichia(msg, color);
+    }
+    else if(msg.action.toString() == "sayYes") {
+	    game.sayYes(color);
+    }
+    else if(msg.action.toString() == "sayNot") {
+	    game.sayNot(color);
+    }
+    else if(msg.action.toString() == "upgrade") {
+        result = game.getUpgrade(color);
+        console.log(result);
+    }
+    return result;
+ };
+
